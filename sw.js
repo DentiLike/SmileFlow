60eaa82deb732a5a4b7b6f8d584245aa90a2106e
DentiLike <dr.alvarado.ortodoncia@gmail.com>
2026-08-01 16:01:56 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* SmileFlow — Service Worker v98 */
-const CACHE = 'smileflow-v98';
+/* SmileFlow — Service Worker v100 */
+const CACHE = 'smileflow-v100';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
